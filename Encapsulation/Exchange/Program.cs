52acd103d8f2a8d50ec1210f84160f91bca93304
Clayman <Clayman@DESKTOP-DM8E77U>
2023-11-16 16:15:34 +0300

    Operators

---
+++ Encapsulation/Exchange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b;
+			Input(out a, out b);
+			Console.WriteLine($"{a}\t{b}");
+			Exchange(ref a, ref b);
+			Console.WriteLine($"{a}\t{b}");
+		}
+		static void Input(out int a, out int b)
+		{
+			Console.Write("Введите два числа: ");
+			a = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+		}
+		static void Exchange(ref int a, ref int b)
+		{
+			int buffer = a;
+			a = b;
+			b = buffer;
+		}
+		//ref - может принять только проиниализированные переменные.
+		//out - может принять неинициализированные переменные, 
+		//		такая функция обычно выполняет инициализацию принятых переменных.
+	}
+}
---
+++ Encapsulation/Point/Program.cs
@@ -0,0 +1,37 @@
+﻿//#define CONSTRUCTORS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if CONSTRUCTORS
+			Point A = new Point();
+			A.SetX(2);
+			A.SetY(3);
+			A.Print();
+
+			A.X = 22;
+			A.Y = 33;
+			A.Print();
+
+			Point B = new Point(700, 800);
+			B.Print(); 
+#endif
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(B - A);
+			A.Print();
+			B.Print();
+			C.Print();
+			++C;
+			C.Print();
+		}
+	}
+}
